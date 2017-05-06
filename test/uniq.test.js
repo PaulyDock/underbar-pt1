6e2c3d25de83443fb3ddb839e86cd57cd2eb5cbc
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-06 16:26:39 -0700

    Add test for uniq

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,9 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of words', () => {
+    const words = ['air', 'bird', 'cat', 'air', 'dog', 'bird', 'elephant'];
+    expect(_.uniq(words)).toEqual(['air', 'bird', 'cat', 'dog', 'elephant']);
+  });
 });
