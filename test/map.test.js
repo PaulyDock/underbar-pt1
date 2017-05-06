434c445df57cccbdd873a6570a8903ea893a00fe
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-06 15:52:46 -0700

    Add test for map

--- test/map.test.js
+++ test/map.test.js
@@ -1,9 +1,18 @@
 _ = require('../underbar');
 
 describe('map()', () => {
-  it('maps every numbers in an array of numbers to their square', () => {
+  it('maps every number in an array of numbers to its square', () => {
     const arr = [1, 2, 3, 4, 5];
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every word in an array of strings to itself in all uppercase', () => {
+    const arr = ['air', 'bird', 'cat', 'dog'];
+    const mappedArr = _.map(arr, function(el) {
+      return el.toUpperCase();
+    });
+    expect(mappedArr).toEqual(['AIR', 'BIRD', 'CAT', 'DOG']);
+  });
+
 });
