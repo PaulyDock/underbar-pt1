54490c62821a78ca75442b06474b65aa0775eb0e
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-06 15:56:55 -0700

    Add tests for pluck

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,24 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array filled with undefined when the tested objects lack the tested property', () => {
+    const people = [
+      { name: 'Harriet' },
+      { name: 'Lazarus' },
+      { name: 'Bethany' }
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([undefined, undefined, undefined]);
+  });
+
+  it('returns an array of just ages, given an array of objects with age as the only property', () => {
+    const people = [
+      { age: 12 },
+      { age: 999},
+      { age: 14 }
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([12, 999, 14]);
+  });
+
 });
